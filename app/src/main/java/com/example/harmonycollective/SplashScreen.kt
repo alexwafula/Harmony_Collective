49f05a5ce47ee680cc1bac269bc72f099c679769
alexwafula <kwobaalex9@gmail.com>
2024-06-05 19:02:34 +0300

    Created splashscreen for the app

---
+++ app/src/main/java/com/example/harmonycollective/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.example.harmonycollective
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // HERE WE ARE TAKING THE REFERENCE OF OUR IMAGE
+        // SO THAT WE CAN PERFORM ANIMATION USING THAT IMAGE
+        val backgroundImage: ImageView = findViewById(R.id.img_logo)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
+        backgroundImage.startAnimation(slideAnimation)
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+}
